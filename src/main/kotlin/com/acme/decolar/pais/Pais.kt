8d0e682b8d5f98b172608a21e57273e70276f429
Júlio Santos <julio.santos@dextra-sw.com>
2020-11-10 20:27:27 -0300

    Implementa JPA

--- src/main/kotlin/com/acme/decolar/pais/Pais.kt
+++ src/main/kotlin/com/acme/decolar/pais/Pais.kt
@@ -1,6 +1,11 @@
 package com.acme.decolar.pais
 
 import java.util.*
+import javax.persistence.Entity
+import javax.persistence.Id
 
-class Pais(val id: UUID = UUID.randomUUID(), val nome: String) {
-}
+@Entity
+class Pais(
+    @Id
+    val id: UUID = UUID.randomUUID(),
+    val nome: String)
--- src/main/kotlin/com/acme/decolar/pais/PaisesController.kt
+++ src/main/kotlin/com/acme/decolar/pais/PaisesController.kt
@@ -1,26 +1,30 @@
 package com.acme.decolar.pais
 
 import org.springframework.http.ResponseEntity
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.util.UriComponentsBuilder
+import javax.persistence.EntityManager
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/admin/paises")
-class PaisesController {
-
-	@PostMapping
-	fun novoPais(@Valid @RequestBody novoPaisRequest: NovoPaisRequest,
-	             uriComponentsBuilder: UriComponentsBuilder ): ResponseEntity<Pais> {
-
-		val model = novoPaisRequest.toModel()
-
-		val path = uriComponentsBuilder.path("/api/admin/paises/${model.id}").build()
-
-		return ResponseEntity.created(path.toUri()).build();
-	}
-
+class PaisesController(
+	val entityManager: EntityManager
+) {
+    
+    @PostMapping
+	@Transactional
+    fun novoPais(
+		@Valid @RequestBody novoPaisRequest: NovoPaisRequest,
+		uriComponentsBuilder: UriComponentsBuilder
+	): ResponseEntity<Pais> {
+        val model = novoPaisRequest.toModel()
+		entityManager.persist(model)
+        val path = uriComponentsBuilder.path("/api/admin/paises/${model.id}").build()
+        return ResponseEntity.created(path.toUri()).build();
+    }
 }
