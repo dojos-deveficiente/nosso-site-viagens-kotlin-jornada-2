0175624547122261e516529f2db61127a06b81a8
Arthur M Fonseca <arthurfnsc@gmail.com>
2020-11-17 20:56:43 -0300

    Adiciona EntityManager

--- src/main/kotlin/com/acme/decolar/compania/UniqueValueValidator.kt
+++ src/main/kotlin/com/acme/decolar/compania/UniqueValueValidator.kt
@@ -1,17 +1,28 @@
 package com.acme.decolar.compania
 
+import javax.persistence.EntityManager
 import javax.validation.ConstraintValidator
 import javax.validation.ConstraintValidatorContext
+import kotlin.reflect.KClass
 
-class UniqueValueValidator:ConstraintValidator<UniqueValue, Any> {
+class UniqueValueValidator(val manager: EntityManager) :ConstraintValidator<UniqueValue, Any> {
+
+    private lateinit var domainClass: KClass<*>
+    private lateinit var field: String
 
     override fun isValid(value: Any?, context: ConstraintValidatorContext?): Boolean {
-        TODO("Not yet implemented")
+
+        if (value == null) {
+            return true
+        }
+
+        manager.createQuery("SELECT 1 FROM ${domainClass.simpleName} WHERE ${field} = :value")
     }
 
     override fun initialize(uniquevalue: UniqueValue?) {
 
         this.domainClass = uniquevalue.domainClass;
+        this.field = uniquevalue.field
 
     }
 
